8ef7042d7009cf08e1bd43b1a3c761f1768ff71b
firza pradipta atalla <pradipta.firza@gmail.com>
2025-08-14 15:36:28 +1000

    Copied IDV60920 + ftp.py for main to xml-parser

---
+++ ftp.py
@@ -0,0 +1,13 @@
+from ftplib import FTP
+
+ftp = FTP('ftp.bom.gov.au')
+ftp.login()  # anonymous login
+ftp.cwd('/anon/gen/fwo')
+
+filename = 'IDV60910.xml'
+
+with open(filename, 'wb') as f:
+    ftp.retrbinary(f'RETR {filename}', f.write)
+
+ftp.quit()
+print(f"Downloaded {filename}")
