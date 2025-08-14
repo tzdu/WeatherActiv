9d5a7ded0613b8b0c7f93819b5b7e86dfdd65061
tzdu <tduu0004@student.monash.edu>
2025-08-14 15:36:56 +1000

    added commments to scraper

--- scraping data/ftp.py
+++ scraping data/ftp.py
@@ -1,13 +1,17 @@
 from ftplib import FTP
 
+# accessing the ftp server
 ftp = FTP('ftp.bom.gov.au')
 ftp.login()  # anonymous login
 ftp.cwd('/anon/gen/fwo')
 
+# the xml file with the data we need
 filename = 'IDV60920.xml'
 
+# opening file
 with open(filename, 'wb') as f:
     ftp.retrbinary(f'RETR {filename}', f.write)
 
+# downloading file
 ftp.quit()
 print(f"Downloaded {filename}")
