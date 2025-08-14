9d84da46508f5f086112548ecc1fb200fd43a617
tzdu <tduu0004@student.monash.edu>
2025-08-14 15:35:18 +1000

    deleted more redundant files and changed the selected xml file to the one with data we need

--- scraping data/ftp.py
+++ scraping data/ftp.py
@@ -4,7 +4,7 @@ ftp = FTP('ftp.bom.gov.au')
 ftp.login()  # anonymous login
 ftp.cwd('/anon/gen/fwo')
 
-filename = 'IDV60910.xml'
+filename = 'IDV60920.xml'
 
 with open(filename, 'wb') as f:
     ftp.retrbinary(f'RETR {filename}', f.write)
